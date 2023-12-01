2aeaab942a0c31d26dd0458672c43228c7a6ed8f
Varez Luniansyah <varezluniansyah08@gmail.com>
2023-12-01 11:37:19 +0700

    upload

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+      <Header />
+      <slot />
+      
+      <Footer class="bg-gradient-to-r from-cyan-500" />
+    </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="grid h-screen place-content-center gap-3 bg-gradient-to-r from-cyan-500">
+      <h1 class="text-5xl text-blue-500 font-sans text-center">Welcome to All Stars Shop</h1>
+      <p class="text-xl text-red-600 text-center">
+        Kalian Bisa Membeli Dan Menjual Produk Di Sini!
+      </p>
+      <NuxtLink href="/Register" class="mx-auto rounded-xl bg-orange-700 px-20 py-4 text-white text-center">
+        Selengkapnya
+      </NuxtLink>
+    </div>
+</template>
+  
